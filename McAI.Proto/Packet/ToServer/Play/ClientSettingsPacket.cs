23aeaa0a45015cc9441e912f9cfc12f6bb5dc2bd
Влад <Vlad-Slender16@yandex.ru>
2020-03-31 16:28:30 +0300

    HeldItemChangePacket ClientSettingsPacket В сервере

---
+++ McAI.Proto/Packet/ToServer/Play/ClientSettingsPacket.cs
@@ -0,0 +1,38 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.ToServer.Play
+{
+    public class ClientSettingsPacket : BasePacket
+    {
+        public override int PacketId => 0x05;
+        public string Locale; //string (16)
+        public byte ViewDistance;
+        public int ChatMode; //Varint Enum
+        public bool ChatColors;
+        public byte DisplayedSkinParts; //Unsigned Byte
+        public int MainHand; //Varint Enum
+
+        public override void Read(byte[] array)
+        {
+            McString.TryParse(ref array, out Locale);
+            McUnsignedByte.TryParse(ref array, out ViewDistance);
+            McInt.TryParse(ref array, out ChatMode);
+            McBoolean.TryParse(ref array, out ChatColors);
+            McUnsignedByte.TryParse(ref array, out DisplayedSkinParts);
+            McInt.TryParse(ref array, out MainHand);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"<[ClientSettings{base.ToString()}] Locale:{Locale} ViewDistance:{ViewDistance} ChatMode:{ChatMode} ChatColors:{ChatColors} DisplayedSkinParts:{DisplayedSkinParts} MainHand:{MainHand}";
+        }
+    }
+}
---
+++ McAI.Proto/Packet/ToServer/Play/HeldItemChangePacket.cs
@@ -0,0 +1,28 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.ToServer.Play
+{
+    public class HeldItemChangePacket : BasePacket
+    {
+        public override int PacketId => 0x23;
+        public short Slot;
+
+        public override void Read(byte[] array)
+        {
+           McShort.TryParse(ref array, out Slot);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"<[HeldItemChange{base.ToString()}] Slot: {Slot}";
+        }
+    }
+}
