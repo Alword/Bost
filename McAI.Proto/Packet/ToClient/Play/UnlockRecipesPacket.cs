c185efd4eec207543f3538a21824e03994d42973
Влад <Vlad-Slender16@yandex.ru>
2020-03-31 15:27:00 +0300

    PluginMessagePacket  - Server UnlockRecipesPacket  - Client

---
+++ McAI.Proto/Packet/ToClient/Play/UnlockRecipesPacket.cs
@@ -0,0 +1,45 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.ToClient.Play
+{
+    public class UnlockRecipesPacket : BasePacket
+    {
+        public override int PacketId => 0x37;
+        public int Action;   //Varint
+        public bool CraftingRecipeBookOpen;
+        public bool CraftingRecipeBookFilterActive;
+        public bool SmeltingRecipeBookOpen;
+        public bool SmeltingRecipeBookFilterActive;
+        public int ArraySize1;  //Varint
+        public string RecipeIDs;  //Array of Identifier
+        public int Arraysize2;    //Optional VarInt
+        public string[] RecipeIDs1;
+        public string[] RecipeIDs2;
+        // Array size 2  Recipe IDs  Number of elements in the following array, only present if mode is 0 (init)
+
+        public override void Read(byte[] array)
+        {
+            McInt.TryParse(ref array, out Action);
+            McBoolean.TryParse(ref array, out CraftingRecipeBookOpen);
+            McBoolean.TryParse(ref array, out CraftingRecipeBookFilterActive);
+            McBoolean.TryParse(ref array, out SmeltingRecipeBookOpen);
+            McBoolean.TryParse(ref array, out SmeltingRecipeBookFilterActive);
+            McInt.TryParse(ref array, out ArraySize1);
+            McString.TryParse(ref array, out RecipeIDs);
+            McInt.TryParse(ref array, out Arraysize2);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[UnlockRecipes{base.ToString()}] Action:{Action} CraftingRecipeBookOpen:{CraftingRecipeBookOpen} CraftingRecipeBookFilterActive:{CraftingRecipeBookFilterActive} SmeltingRecipeBookOpen:{SmeltingRecipeBookOpen} SmeltingRecipeBookFilterActive:{SmeltingRecipeBookFilterActive} ArraySize1:{ArraySize1} RecipeIDs:{RecipeIDs} Arraysize2:{Arraysize2}";
+        }
+    }
+}
---
+++ McAI.Proto/Packet/ToServer/Play/PluginMessagePacket.cs
@@ -0,0 +1,30 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.ToServer.Play
+{
+    public class PluginMessagePacket : BasePacket
+    {
+        public override int PacketId => 0x0B;
+        public string Channel; //Identifier
+        public byte Data; //Byte Array
+
+        public override void Read(byte[] array)
+        {
+            McString.TryParse(ref array, out Channel);
+            McUnsignedByte.TryParse(ref array, out Data);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"<[PluginMessage{base.ToString()}] Channel: {Channel} Data: {Data}";
+        }
+    }
+}
