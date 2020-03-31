402442328393bd1018b8011670e6940be6c3999b
Alword <anzer987@yandex.ru>
2020-03-31 14:23:47 +0300

    rename commands

---
+++ McAI.Proto/Commands/ToClient/Play/EntityStatus.cs
@@ -0,0 +1,22 @@
+﻿using McAI.Proto.Packet.ToClient.Play;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Commands.ToClient.Play
+{
+    public class EntityStatus : Command
+    {
+        private readonly EntityStatusPacket entityStatusPacket;
+        public EntityStatus(bool isLogging) : base(isLogging)
+        {
+            entityStatusPacket = new EntityStatusPacket();
+        }
+
+        public override void Execute(byte[] array)
+        {
+            entityStatusPacket.Read(array);
+            Debug(entityStatusPacket.ToString());
+        }
+    }
+}
