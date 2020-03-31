a2deccbec933f4619ef19193aaf142f2934425b8
Alword <anzer987@yandex.ru>
2020-03-31 21:33:18 +0300

    SpawnWeatherEntity

---
+++ McAI.Proto/Packet/Play/Clientbound/SpawnWeatherEntity.cs
@@ -0,0 +1,44 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Clientbound
+{
+    public class SpawnWeatherEntity : BasePacket
+    {
+        public override int PacketId => 0x01;
+
+        public int EntityId; // varing
+        public byte Type;
+        public double X;
+        public double Y;
+        public double Z;
+
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out EntityId);
+            McUnsignedByte.TryParse(ref array, out Type);
+            McDouble.TryParse(ref array, out X);
+            McDouble.TryParse(ref array, out Y);
+            McDouble.TryParse(ref array, out Z);
+        }
+
+        public override byte[] Write()
+        {
+            List<byte> bytes = new List<byte>();
+            bytes.AddRange(McVarint.ToBytes(EntityId));
+            bytes.AddRange(McUnsignedByte.ToBytes(Type));
+            bytes.AddRange(McDouble.ToBytes(X));
+            bytes.AddRange(McDouble.ToBytes(Y));
+            bytes.AddRange(McDouble.ToBytes(Z));
+            return bytes.ToArray();
+        }
+
+        public override string ToString()
+        {
+            return $"<[SpawnExperienceOrb] EntityId:{EntityId} Type: {Type} X:{X} Y:{Y} Z:{Z}";
+        }
+    }
+}
