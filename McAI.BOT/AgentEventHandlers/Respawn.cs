6b95785c1fbeffd66d9b84eeec1b49a85b46757c
Alword <anzer987@yandex.ru>
2020-04-02 16:46:22 +0300

    respawn

---
+++ McAI.BOT/AgentEventHandlers/Respawn.cs
@@ -0,0 +1,39 @@
+﻿using McAI.Proto.Enum;
+using McAI.Proto.Packet;
+using McAI.Proto.Packet.Play.Clientbound;
+using McAI.Proto.Packet.Play.Serverbound;
+using McAI.Proto.StreamReader.Abstractions;
+using McAI.Proto.StreamReader.Enum;
+using McAI.Proto.StreamReader.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace McAI.BOT.AgentEventHandlers
+{
+    public class Respawn : IPacketEventHandler
+    {
+        private readonly Agent agent;
+        public Respawn(Agent agent)
+        {
+            this.agent = agent;
+        }
+        public async void OnPacket(PacketKey state, BasePacket packet)
+        {
+            var health = (UpdateHealthPacket)packet;
+
+            if (health.Health <= 0)
+            {
+                var clientStatus = new ClientStatusPacket
+                {
+                    ActionID = ClientStatusActions.PerformPespawn
+                };
+
+                List<byte> send = new List<byte>();
+                agent.Write(clientStatus, true, send);
+                await agent.Send(send.ToArray());
+            }
+
+        }
+    }
+}
--- McAI.BOT/Program.cs
+++ McAI.BOT/Program.cs
@@ -16,6 +16,7 @@ namespace McAI.BOT
             agent.OnRecive += connectionListner.ReciveListner;
             agent.OnSend += connectionListner.SendListner;
             connectionListner.Subscribe(new PacketKey(0x21, ConnectionStates.Play, Bounds.Client), new KeepAlive(agent));
+            connectionListner.Subscribe(new PacketKey(0x49, ConnectionStates.Play, Bounds.Client), new Respawn(agent));
 
             Run(agent).GetAwaiter().GetResult();
 
