3bb8d518d4a12438cf7b57c612a5fc26e8ac691d
Alword <anzer987@yandex.ru>
2020-04-02 14:18:35 +0300

    update agent

---
+++ McAI.BOT/AgentEventHandlers/KeepAlive.cs
@@ -0,0 +1,22 @@
+﻿using McAI.Proto.Packet;
+using System.Collections.Generic;
+
+namespace McAI.BOT.AgentEventHandlers
+{
+    public class KeepAlive
+    {
+        public async void OnPacket(object sender, BasePacket packet)
+        {
+            var agent = (Agent)sender;
+            var toclientKeepAlive = (Proto.Packet.Play.Clientbound.KeepAlivePacket)packet;
+
+            var toServerKeepAlive = new Proto.Packet.Play.Serverbound.KeepAlivePacket
+            {
+                KeepAliveID = toclientKeepAlive.KeepAliveID
+            };
+            List<byte> send = new List<byte>();
+            agent.Write(toServerKeepAlive, true, send);
+            await agent.Send(send.ToArray());
+        }
+    }
+}
