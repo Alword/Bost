60aeb13e24a6d969af76f9f25a7209b904d70bf3
Влад <Vlad-Slender16@yandex.ru>
2020-04-04 14:26:27 +0300

    Entity Action

---
+++ McAI.Proto/Packet/Play/Serverbound/EntityActionPacket.cs
@@ -0,0 +1,33 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Serverbound
+{
+    public class EntityActionPacket : BasePacket
+    {
+        public override int PacketId => 0x1B;
+
+        public int EntityID; //Varint
+        public int ActionID; //Varint Enum
+        public int JumpBoost; //Varint
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out EntityID);
+            McVarint.TryParse(ref array, out ActionID);
+            McVarint.TryParse(ref array, out JumpBoost);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[EntityAction] EntityID:{EntityID} ActionID:{ActionID} JumpBoost:{JumpBoost}";
+        }
+    }
+}
