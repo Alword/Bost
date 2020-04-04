5285c36f27d33a35ff505addc81eba099b44a781
Влад <Vlad-Slender16@yandex.ru>
2020-04-04 14:06:12 +0300

    Player Abilities

---
+++ McAI.Proto/Packet/Play/Serverbound/PlayerAbilitiesPacket.cs
@@ -0,0 +1,33 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Serverbound
+{
+    public class PlayerAbilitiesPacket : BasePacket
+    {
+        public override int PacketId => 0x19;
+
+        public byte Flags;
+        public float FlyingSpeed;
+        public float WalkingSpeed;
+
+        public override void Read(byte[] array)
+        {
+            McUnsignedByte.TryParse(ref array, out Flags);
+            McFloat.TryParse(ref array, out FlyingSpeed);
+            McFloat.TryParse(ref array, out WalkingSpeed);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[PlayerAbilities] Flags:{Flags} FlyingSpeed:{FlyingSpeed} WalkingSpeed:{WalkingSpeed}";
+        }
+    }
+}
