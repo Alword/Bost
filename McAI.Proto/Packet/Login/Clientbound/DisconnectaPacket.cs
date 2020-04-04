8ed55bdb78512886b76ff1a14311f1a846309271
Влад <Vlad-Slender16@yandex.ru>
2020-04-04 14:52:06 +0300

    Disconnect

---
+++ McAI.Proto/Packet/Login/Clientbound/DisconnectaPacket.cs
@@ -0,0 +1,23 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Login.Clientbound
+{
+    public class DisconnectaPacket : BasePacket
+    {
+        public override int PacketId => 0x00;
+        public string Reason; //Chat
+
+        public override void Read(byte[] array)
+        {
+            McString.TryParse(ref array, out Reason);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
