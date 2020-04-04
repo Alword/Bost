09b44e859ea38bcf0e800210c2a731445b09fa4f
Влад <Vlad-Slender16@yandex.ru>
2020-04-04 15:22:26 +0300

    Encryption Response

---
+++ McAI.Proto/Packet/Login/Serverbound/EncryptionResponsePacket.cs
@@ -0,0 +1,30 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Login.Serverbound
+{
+    public class EncryptionResponsePacket : BasePacket
+    {
+        public override int PacketId => 0x01;
+
+        public int SharedSecretLength; //Varint
+        public byte[] SharedSecret; //Byte Array
+        public int VerifyTokenLength; //Varint
+        public byte[] VerifyToken; //Byte Array
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out SharedSecretLength);
+            McByteArray.TryParse(ref array, out SharedSecret);
+            McVarint.TryParse(ref array, out VerifyTokenLength);
+            McByteArray.TryParse(ref array, out VerifyToken);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
