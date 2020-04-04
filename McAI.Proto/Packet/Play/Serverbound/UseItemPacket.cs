428b2f8a6cb3afd631e5a3bcbe54a20e5c350245
Влад <Vlad-Slender16@yandex.ru>
2020-04-04 14:39:29 +0300

    Use Item

---
+++ McAI.Proto/Packet/Play/Serverbound/UseItemPacket.cs
@@ -0,0 +1,28 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Serverbound
+{
+    public class UseItemPacket : BasePacket
+    {
+        public override int PacketId => 0x2D;
+        public int Hand; //Varint Enum
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out Hand);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[UseItem] Hand:{Hand}";
+        }
+    }
+}
