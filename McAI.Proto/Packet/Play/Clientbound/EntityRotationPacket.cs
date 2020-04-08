659a6e1ae1eb9078bbcd9c43d16aa22a2126104e
Влад <Vlad-Slender16@yandex.ru>
2020-04-08 13:36:11 +0300

    Entity Rotation

---
+++ McAI.Proto/Packet/Play/Clientbound/EntityRotationPacket.cs
@@ -0,0 +1,35 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Clientbound
+{
+    public class EntityRotationPacket : BasePacket
+    {
+        public override int PacketId => 0x2B;
+
+        public int EntityID; //Varint
+        public byte Yaw; //Angle
+        public byte Pitch; //Angle
+        public bool OnGround;
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out EntityID);
+            McUnsignedByte.TryParse(ref array, out Yaw);
+            McUnsignedByte.TryParse(ref array, out Pitch);
+            McBoolean.TryParse(ref array, out OnGround);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[EntityRotation] EntityID:{EntityID} Yaw:{Yaw} Pitch:{Pitch} OnGround:{OnGround}";
+        }
+    }
+}
