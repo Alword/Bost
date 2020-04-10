137d2d1bfa9d38aab88228903dda657e0d114f8a
Влад <Vlad-Slender16@yandex.ru>
2020-04-10 14:09:17 +0300

    Collect Item

---
+++ McAI.Proto/Packet/Play/Clientbound/CollectItemPacket.cs
@@ -0,0 +1,34 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Clientbound
+{
+    public class CollectItemPacket : BasePacket
+    {
+        public override int PacketId => 0x56;
+
+        public int CollectedEntityID; //Varint
+        public int CollectorEntityID; //Varint
+        public int PickupItemCount; //Varint
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out CollectedEntityID);
+            McVarint.TryParse(ref array, out CollectorEntityID);
+            McVarint.TryParse(ref array, out PickupItemCount);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[CollectItem] CollectedEntityID:{CollectedEntityID} " +
+                $"CollectorEntityID:{CollectorEntityID} PickupItemCount:{PickupItemCount}";
+        }
+    }
+}
---
+++ McAI.Proto/Packet/Play/Clientbound/DestroyEntitiesPacket.cs
@@ -0,0 +1,39 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Clientbound
+{
+    public class DestroyEntitiesPacket : BasePacket
+    {
+        public override int PacketId => 0x38;
+
+        public int Count; //Varint
+        public int[] EntityIDs; //Array of VarInt
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out Count);
+           /* for ( int i = 0; i < Count; i++)
+            {
+                McVarint.TryParse(ref array, out EntityIDs[i]);
+            }*/
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+       /* public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (int entity in EntityIDs)
+            {
+                stringBuilder.AppendLine(entity.ToString());
+            }
+            return $"[PlayerInfo] Count:{Count} EntityIDs:{stringBuilder}";
+        }*/
+    }
+}
