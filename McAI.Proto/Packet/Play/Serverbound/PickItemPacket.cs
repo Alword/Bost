fa4b7f608f6cf0325d2cfcc13fc15231ebb27ea8
Влад <Vlad-Slender16@yandex.ru>
2020-04-10 12:46:01 +0300

    Pick Item

---
+++ McAI.Proto/Packet/Play/Serverbound/PickItemPacket.cs
@@ -0,0 +1,28 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Serverbound
+{
+    public class PickItemPacket : BasePacket
+    {
+        public override int PacketId => 0x17;
+        public int SlotToUse; //Varint
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out SlotToUse);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[PickItem] SlotToUse:{SlotToUse}";
+        }
+    }
+}
