646acd692fa01235bddd831297da8d5b5e86a337
Влад <Vlad-Slender16@yandex.ru>
2020-04-10 20:02:11 +0300

    Open Window

---
+++ McAI.Proto/Packet/Play/Clientbound/OpenWindowPacket.cs
@@ -0,0 +1,34 @@
+﻿using McAI.Proto.Model.ChatObject;
+using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Clientbound
+{
+    public class OpenWindowPacket : BasePacket
+    {
+        public override int PacketId => 0x2F;
+
+        public int WindowID; //Varint
+        public int WindowType; //Varint
+        public Chat WindowTitle;
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out WindowID);
+            McVarint.TryParse(ref array, out WindowType);
+            McChat.TryParse(ref array, out WindowTitle);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[OpenWindow] WindowID:{WindowID} WindowType:{WindowType} WindowTitle:{WindowTitle}";
+        }
+    }
+}
