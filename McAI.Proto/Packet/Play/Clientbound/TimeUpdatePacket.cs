509932df495fd8b39e9eb6bb0416a17847bf9887
Влад <Vlad-Slender16@yandex.ru>
2020-04-12 11:41:16 +0300

    Time Update

---
+++ McAI.Proto/Packet/Play/Clientbound/TimeUpdatePacket.cs
@@ -0,0 +1,31 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Clientbound
+{
+    public class TimeUpdatePacket : BasePacket
+    {
+        public override int PacketId => 0x4F;
+
+        public long WorldAge;
+        public long TimeOfDay;
+
+        public override void Read(byte[] array)
+        {
+            McLong.TryParse(ref array, out WorldAge);
+            McLong.TryParse(ref array, out TimeOfDay);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[TimeUpdate] WorldAge:{WorldAge} TimeOfDay:{TimeOfDay}";
+        }
+    }
+}
