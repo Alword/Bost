272560329bee1e268ffa35071326ba286c4ec29e
Влад <Vlad-Slender16@yandex.ru>
2020-04-12 11:12:20 +0300

    Update View Position

---
+++ McAI.Proto/Packet/Play/Clientbound/UpdateViewPositionPacket.cs
@@ -0,0 +1,31 @@
+﻿using McAI.Proto.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McAI.Proto.Packet.Play.Clientbound
+{
+    public class UpdateViewPositionPacket : BasePacket
+    {
+        public override int PacketId => 0x41;
+
+        public int ChunkX; //Varint
+        public int ChunkZ; //Varint
+
+        public override void Read(byte[] array)
+        {
+            McVarint.TryParse(ref array, out ChunkX);
+            McVarint.TryParse(ref array, out ChunkZ);
+        }
+
+        public override byte[] Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"[UpdateViewPosition] ChunkX:{ChunkX} ChunkZ:{ChunkZ}";
+        }
+    }
+}
