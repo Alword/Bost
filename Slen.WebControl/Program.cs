5d6fab589a79efd9ca029436653132cd4d3766b0
Alword <anzer987@yandex.ru>
2020-07-28 18:44:57 +0300

    update brand name

--- Slen.WebControl/Program.cs
+++ Slen.WebControl/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Globalization;
+using Microsoft.JSInterop;
 
 namespace Slen.WebControl
 {
@@ -16,10 +18,21 @@ namespace Slen.WebControl
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddLocalization();
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+
+            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+            var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+            if (result != null)
+            {
+                var culture = new CultureInfo(result);
+                CultureInfo.DefaultThreadCurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentUICulture = culture;
+            }
+
+            await host.RunAsync();
         }
     }
 }
