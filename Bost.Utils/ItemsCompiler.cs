64aef168c244256bbc108542eb0ebdc92830be98
Alword <anzer987@yandex.ru>
2020-08-18 16:33:19 +0300

    feature minecraft item icons compiler

--- Bost.Utils/ItemsCompiler.cs
+++ Bost.Utils/ItemsCompiler.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Bost.Utils
 {
@@ -15,45 +17,80 @@ namespace Bost.Utils
             this.imagePath = path;
         }
 
-        public void Compile()
+        public async Task Download()
         {
-            string format = ".png";
-            int itemSize = 32;
-            StringBuilder stringBuilder = new StringBuilder();
-            var files = Directory.GetFiles(imagePath).Where(d => d.EndsWith(format)).ToArray();
-            int inlineCount = (int)Math.Ceiling(Math.Sqrt(files.Length));
-            int atlasSize = itemSize * inlineCount;
-            stringBuilder.AppendLine($"{itemSize}");
-            stringBuilder.AppendLine($"{inlineCount}");
-
-            using (var bitmap = new Bitmap(atlasSize, atlasSize))
+            using (HttpClient client = new HttpClient())
             {
-                int i = 0;
-                int j = 0;
-                using (Graphics g = Graphics.FromImage(bitmap))
+                var itemsTask = GetItems(client);
+                var pointsTask = GetPoints(client);
+                Dictionary<string, string> itemIdItem = await itemsTask.ConfigureAwait(false);
+                List<ItemPos> points = await pointsTask.ConfigureAwait(false);
+
+                int itemSize = 32;
+                int inlineCount = 28;
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine($"{itemSize}");
+                stringBuilder.AppendLine($"{inlineCount}");
+                foreach (var p in points)
                 {
-                    foreach (var file in files)
-                    {
-                        string name = Path.GetFileName(file).Replace(format, "");
-                        stringBuilder.AppendLine(name);
-                        var image = Image.FromFile(file);
-                        g.DrawImage(image, i, j, itemSize, itemSize);
-                        i += itemSize;
-                        if (i > atlasSize)
-                        {
-                            j += itemSize;
-                            i = 0;
-                        }
-                    }
-                    g.Save();
+                    stringBuilder.AppendLine($"{itemIdItem[p.Item]}");
                 }
-                bitmap.Save("items.png");
                 File.WriteAllText("items.txt", stringBuilder.ToString());
             }
         }
 
+        private static async Task<Dictionary<string, string>> GetItems(HttpClient client)
+        {
+            var content = client.GetStringAsync("https://minecraft-ids.grahamedgecombe.com/");
+
+            var contentResult = await content;
+
+            Dictionary<string, string> itemIdItem = new Dictionary<string, string>();
+            var items = contentResult.Split("class=\"row\">")[1..];
+            foreach (var item in items)
+            {
+                var split = item.Split(">");
+                var name = split[8].Split("<")[0].Replace("(", "").Replace(")", "");
+                var item_id = split[3].Split(" ")[2].Split("\"")[0];
+                itemIdItem.Add(item_id, name);
+            }
+
+            return itemIdItem;
+        }
+
+        private static async Task<List<ItemPos>> GetPoints(HttpClient client)
+        {
+            var css = client.GetStringAsync("https://minecraft-ids.grahamedgecombe.com/stylesheets/bundles/all/1534075872.css");
+            var cssResult = await css;
+            var itemsPos = cssResult.Split(".items-")[1..];
+            List<ItemPos> points = new List<ItemPos>(itemsPos.Length);
+            foreach (var pos in itemsPos)
+            {
+                string name = $"items-{pos.Split("{")[0]}";
+                var spaces = pos.Split(" ");
+                int x = int.Parse(spaces[1].Replace("px", ""));
+                int y = int.Parse(spaces[2].Replace("px", ""));
+                points.Add(new ItemPos(name, x, y));
+            }
+            points = points.OrderByDescending(d => d.Y).ThenByDescending(d => d.X).ToList();
+            return points;
+        }
+
         public void Dispose()
         {
+        }
+
+        readonly struct ItemPos
+        {
+            public ItemPos(string item, int x, int y)
+            {
+                this.Item = item;
+                this.X = x;
+                this.Y = y;
+            }
+            public string Item { get; }
+            public int X { get; }
+            public int Y { get; }
         }
     }
 }
--- Bost.Utils/Program.cs
+++ Bost.Utils/Program.cs
@@ -13,23 +13,13 @@ namespace Bost.Utils
     {
         static void Main(string[] args)
         {
-            Action action = Action.Test;
+            Action action = Action.CompileImage;
             switch (action)
             {
                 case Action.CompileImage:
                     CompileImage();
                     break;
-                case Action.Test:
-                    Test();
-                    break;
             }
-        }
-        private static void Test()
-        {
-            string path = "AppData/oak_log.png";
-            string top = "AppData/oak_log_top.png";
-            ImageRotator imageRotator = new ImageRotator();
-            imageRotator.GetLeftSide(path, top);
         }
         private static void CompileImage()
         {
@@ -37,7 +27,7 @@ namespace Bost.Utils
             string path = @"C:\Users\Artyom\Downloads\1\assets\minecraft\textures\item";
             using (var itemsCompiler = new ItemsCompiler(path))
             {
-                itemsCompiler.Compile();
+                itemsCompiler.Download().GetAwaiter().GetResult();
             }
         }
     }
